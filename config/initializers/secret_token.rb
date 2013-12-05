2ff60334abd167a9b993a52242aa15d62d363334
Rob Watkin <robwatkin@gmail.com>
2013-12-05 20:26:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rapto::Application.config.secret_key_base = '27c12ce3f000405ec00687ad7d2e48aca774e05be7e7f78eb1b59592162be0a63878cc4aeedd78d96343d863bded8707181c82ee99980d1c38a4f5df97f77245'
